d98876370797dadb22b37520c8b7cbf41ecc1a5e
jusko1505 <97807951+jusko1505@users.noreply.github.com>
2022-01-22 16:30:23 -0800

    Add files via upload

---
+++ source code/passMain.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class passMain
+{
+    public static void main(String[] args) throws IOException
+    {
+        passGui passGui = new passGui();
+        passGui.createGui();
+    }
+}
